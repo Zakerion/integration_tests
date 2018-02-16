8f31beaa3bffbc6671737dd22625106755dfee46
Maciej Przyjemski <180264@edu.p.lodz.pl>
2018-02-16 18:12:18 +0100

    3.3 dodanie testu dla http ze statusem 409

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -1,5 +1,7 @@
 package edu.iis.mto.blog.api;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -10,6 +12,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -42,7 +45,7 @@ public class BlogApiTest {
         user.setEmail("john@domain.com");
         user.setFirstName("John");
         user.setLastName("Steward");
-        Mockito.when(blogService.createUser(user)).thenReturn(newUserId);
+        when(blogService.createUser(user)).thenReturn(newUserId);
         String content = writeJson(user);
 
         mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -50,6 +53,12 @@ public class BlogApiTest {
                 .andExpect(content().string(writeJson(new Id(newUserId))));
     }
 
+    @Test
+    public void exceptionShouldResponseWithStatus409() throws Exception {
+    	when(finder.getUserData(2L)).thenThrow(new DataIntegrityViolationException("conflict"));
+    	mvc.perform(get("/blog/user/2")).andExpect(status().isConflict());
+    }
+    
     private String writeJson(Object obj) throws JsonProcessingException {
         return new ObjectMapper().writer().writeValueAsString(obj);
     }
